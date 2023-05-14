07b943dd18ba253ab12739667887a117a47d0dc6
SehrawatRahul <90616206+SehrawatRahul@users.noreply.github.com>
2023-05-15 00:36:21 +0530

    Add files via upload

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        <a>
+          Rahul Sehrawat 
+        </a>
+      </section>
+
+      
+    </footer>
+  );
+};
+
+export default Footer;
